a7888e2cfbf905a8dd00398b8b8ec41537b45622
Jeremy Shantz <jeremy@shantz.io>
2014-12-31 14:11:39 -0500

    .

---
+++ optimizer.py
@@ -0,0 +1,10 @@
+import sys
+import os
+def test(message):
+	f = open(os.path.expanduser('~/') + 'dev/portfolio-sim/output.txt', mode = 'at', encoding = 'utf-8' )
+	f.write(message + '\n')
+	f.close()
+
+
+
+test('hello')
